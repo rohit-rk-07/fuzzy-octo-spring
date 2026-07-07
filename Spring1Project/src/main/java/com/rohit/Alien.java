1972132234473a5af1e9d23c9d458411307b4017
rohit-rk-7 <rohitkshirsagar919@gmail.com>
2026-07-07 19:14:36 +0530

    walking through spring framework

---
+++ Spring1Project/src/main/java/com/rohit/Alien.java
@@ -0,0 +1,23 @@
+package com.rohit;
+
+public class Alien {
+	
+	private int age;
+	
+	public Alien() {
+		System.out.println("Object created!");
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void code() {
+		System.out.println("Coding");
+	}
+
+}
---
+++ Spring1Project/src/main/java/com/rohit/App.java
@@ -0,0 +1,17 @@
+package com.rohit;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Alien obj = (Alien) context.getBean("alien");
+//		obj.code();
+//		obj.setAge(21);
+		System.out.println(obj.getAge());
+	}
+
+}
