5817a012282912e181a4c950cee0b644bc2c750e
rohit-rk-7 <rohitkshirsagar919@gmail.com>
2026-07-18 15:45:10 +0530

    web mvc thing without the SPRING BOOT, ie with the maven and spring MVC

---
+++ SpringMVCdemo/src/main/java/com/rohit/SpringMVCDemo/HomeController.java
@@ -0,0 +1,36 @@
+package com.rohit.SpringMVCDemo;
+
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("home method called");
+		return "index";
+	}
+	
+	@RequestMapping("add")
+	public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2 , ModelAndView mv) {
+
+		int result = num1 + num2;
+		
+		mv.addObject("result", result);
+		mv.setViewName("result");
+		
+		return mv;
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute Alien alien) {
+		
+		return "result";
+	}
+}
